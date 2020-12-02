374e9f09fcb5146dcb8e20396c4996cc8ce8ecd7
Israel Ekpo <israel.ekpo@gmail.com>
2020-12-02 15:30:47 -0500

    Saving changes

--- src/main/java/com/izzyacademy/services/ProductEnrichmentService.java
+++ src/main/java/com/izzyacademy/services/ProductEnrichmentService.java
@@ -1,8 +1,9 @@
 package com.izzyacademy.services;
 
-import estreams66.ecommerce.product_details.Value;
+import estreams64.ecommerce.product_details.Value;
 import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import org.apache.commons.compress.utils.Lists;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
@@ -18,12 +19,12 @@ import static java.util.Collections.singletonMap;
 
 public class ProductEnrichmentService implements KafkaStreamService{
 
-    private static final String CLIENT_ID = "66";
+    private static final String CLIENT_ID = "64";
     private static final String APP_ID = "product_enrichment_services";
 
-    private static final String PRODUCT_TOPIC = "estreams66.ecommerce.products";
-    private static final String PRODUCT_DETAILS_TOPIC = "estreams66.ecommerce.product_details";
-    private static final String PRODUCT_ENRICHED2_TOPIC = "estreams66.ecommerce.products_enriched2";
+    private static final String PRODUCT_TOPIC = "estreams64.ecommerce.products";
+    private static final String PRODUCT_DETAILS_TOPIC = "estreams64.ecommerce.product_details";
+    private static final String PRODUCT_ENRICHED2_TOPIC = "estreams64.ecommerce.products_enriched";
 
     public ProductEnrichmentService()
     {
@@ -34,34 +35,42 @@ public class ProductEnrichmentService implements KafkaStreamService{
     public void run() {
 
         // Pass in via ENVIRONMENT variables
-        final String schemaRegistryUrl = "http://schemaregistry-external:8081";
-        final String bootStrapServer = "broker1.izzyacademy.com:9093,broker2.izzyacademy.com:9093,broker3.izzyacademy.com:9093,broker4.izzyacademy.com:9093,broker5.izzyacademy.com:9093";
+        final String schemaRegistryUrl = "http://schemaregistry-external.river.svc.cluster.local:8081";
+        final String[] boostrapServerList = {
+                "broker1-external.river.svc.cluster.local:9093",
+                "broker2-external.river.svc.cluster.local:9093",
+                "broker3-external.river.svc.cluster.local:9093",
+                "broker4-external.river.svc.cluster.local:9093",
+                "broker5-external.river.svc.cluster.local:9093",
+        };
+
+        final String bootStrapServers = String.join(",", boostrapServerList);
 
         final Properties streamsConfiguration = new Properties();
         streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, CLIENT_ID);
         streamsConfiguration.put(StreamsConfig.CLIENT_ID_CONFIG, APP_ID);
-        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServer);
+        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServers);
         streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
         Map<String, String> serdeConfig = singletonMap(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
 
 
-        final SpecificAvroSerde<estreams66.ecommerce.products.Value> productSerde = new SpecificAvroSerde<>();
+        final SpecificAvroSerde<estreams64.ecommerce.products.Value> productSerde = new SpecificAvroSerde<>();
         productSerde.configure(serdeConfig, false);
 
-        final SpecificAvroSerde<estreams66.ecommerce.product_details.Value> productDetailsSerde = new SpecificAvroSerde<>();
+        final SpecificAvroSerde<estreams64.ecommerce.product_details.Value> productDetailsSerde = new SpecificAvroSerde<>();
         productDetailsSerde.configure(serdeConfig, false);
 
-        final SpecificAvroSerde<estreams66.ecommerce.product_enriched.Value> productEnrichedSerde = new SpecificAvroSerde<>();
+        final SpecificAvroSerde<estreams64.ecommerce.product_enriched.Value> productEnrichedSerde = new SpecificAvroSerde<>();
         productEnrichedSerde.configure(serdeConfig, false);
 
         StreamsBuilder builder = new StreamsBuilder();
 
-        KStream<Integer, estreams66.ecommerce.products.Value> productStream = builder.stream(PRODUCT_TOPIC, Consumed.with(Serdes.Integer(), productSerde));
+        KStream<Integer, estreams64.ecommerce.products.Value> productStream = builder.stream(PRODUCT_TOPIC, Consumed.with(Serdes.Integer(), productSerde));
 
-        KStream<Integer, estreams66.ecommerce.product_details.Value> productDetailsTable = builder.stream(PRODUCT_DETAILS_TOPIC, Consumed.with(Serdes.Integer(), productDetailsSerde));
+        KStream<Integer, estreams64.ecommerce.product_details.Value> productDetailsTable = builder.stream(PRODUCT_DETAILS_TOPIC, Consumed.with(Serdes.Integer(), productDetailsSerde));
 
-        KStream<Integer, estreams66.ecommerce.product_enriched.Value> outputStream = productStream.join(productDetailsTable,
+        KStream<Integer, estreams64.ecommerce.product_enriched.Value> outputStream = productStream.join(productDetailsTable,
           (productValue, productDetailValue) -> joiner(productValue, productDetailValue),
               JoinWindows.of(Duration.ofSeconds(5)),
             StreamJoined.with(Serdes.Integer(), productSerde, productDetailsSerde));
@@ -77,9 +86,9 @@ public class ProductEnrichmentService implements KafkaStreamService{
         Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
     }
 
-    private static estreams66.ecommerce.product_enriched.Value joiner(estreams66.ecommerce.products.Value product, estreams66.ecommerce.product_details.Value details) {
+    private static estreams64.ecommerce.product_enriched.Value joiner(estreams64.ecommerce.products.Value product, estreams64.ecommerce.product_details.Value details) {
 
-        estreams66.ecommerce.product_enriched.Value result = new estreams66.ecommerce.product_enriched.Value();
+        estreams64.ecommerce.product_enriched.Value result = new estreams64.ecommerce.product_enriched.Value();
 
         result.setProductId(product.getProductId());
         result.setName(product.getName());
